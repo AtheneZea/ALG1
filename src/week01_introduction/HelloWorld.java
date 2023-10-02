6c8b91532f02e03039e652174d258d2b721835eb
Jaryna Kozak <jaryna.kozak@tul.cz>
2023-10-02 12:22:30 +0200

    Add new commit

---
+++ src/week01_introduction/HelloWorld.java
@@ -0,0 +1,25 @@
+
+package week01_introduction;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+      
+      
+      
+int rokNarozeni = 1980;
+
+int cislo1; 			 
+cislo1 = 8;
+cislo1 = cislo1 + 1;
+int cislo2 = 4;
+cislo2 = cislo1 + cislo2;
+
+
+
+
+
+    }
+    
+}
